3240797433c6dfa71f53b60234aa2fd1e3688c74
AndrewVorobey <andrew_vorobey@mail.ru>
2014-03-03 18:13:36 +0400

    Пустой проект dll.

---
+++ Matrics_Functions/Matrics_Functions/Matrics_Functions.cpp
@@ -0,0 +1,15 @@
+// Matrics_Functions.cpp: определяет экспортированные функции для приложения DLL.
+//
+
+#include "stdafx.h"
+
+namespace Matrix_Functions
+{
+	//TODO
+	//
+	// В этом месте будет реализован интерфейс программы. 
+	//
+	//
+	
+}
+
