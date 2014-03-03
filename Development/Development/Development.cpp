f874c0406134f350bbdf93abff11f4ce75516337
AndrewVorobey <andrew_vorobey@mail.ru>
2014-03-03 18:50:30 +0400

    Добавлен проект разработки - он поможет разрабатывать и отлаживать алгоритмы библеотеки, не требуя каждый раз собирать библиотеку.

---
+++ Development/Development/Development.cpp
@@ -0,0 +1,16 @@
+// Development Данный проект позволяет разработчика запрограммировать сущность программы
+//Но, сборка Dll остается "за кадром"
+
+#include "stdafx.h"
+#include <iostream>
+#include "..\..\Matrics_Functions\Matrics_Functions\Matrics_Functions.cpp"
+
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
+
