dfe74545e685e228272febd9ba97531cb05b4507
ViktoriyaDanilova <realy_love@mail.ru>
2014-03-22 12:33:44 +0400

    Исправлен тест " Доступ к элементу вектора"

--- unitTests/unitTests/unitTests.cpp
+++ unitTests/unitTests/unitTests.cpp
@@ -5,7 +5,6 @@
 #include <math.h>
 #include <iostream>
 #include "../../Matrics_Functions/Matrics_Functions/Matrics_Functions.cpp"
-
 //В этом файле хранится файл для тестирования. Он будет активен при сливании. 
 
 using std::cout;
@@ -13,6 +12,24 @@ using std::cin;
 
 
 void tests();
+void testVector1();
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	tests();
+	int a;
+	cin>>a;
+	Toplitz_Matrix::Vector* vec = new Toplitz_Matrix::RamVector();
+	return 0;
+}
+
+void tests()
+{
+	testVector1();
+	//тут должны быть вызваны функции-юнит тестов.
+	//Каждый юнит тест должен быть в отдельной функции. Тут только вызовы. 
+}
 void testVector1()
 {
 	Toplitz_Matrix::Vector* vec1 = new Toplitz_Matrix::RamVector();
@@ -31,22 +48,5 @@ void testVector1()
 		
 	}
 	if(f) cout <<"Проверка доступа к элементу вектора прошла успешно";
-}
-
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-	tests();
-	int a;
-	cin>>a;
-	Toplitz_Matrix::Vector* vec = new Toplitz_Matrix::RamVector();
-	return 0;
-}
-
-void tests()
-{
-	testVector1();
-	//тут должны быть вызваны функции-юнит тестов.
-	//Каждый юнит тест должен быть в отдельной функции. Тут только вызовы. 
 }
 
