70b40987689c010a93a63e5733f14729907b5dea
ViktoriyaDanilova <realy_love@mail.ru>
2014-03-22 11:39:48 +0400

    Добавлен тест доступа к элементу вектора

--- unitTests/unitTests/unitTests.cpp
+++ unitTests/unitTests/unitTests.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <iostream>
 #include "../../Matrics_Functions/Matrics_Functions/Matrics_Functions.cpp"
+
 //В этом файле хранится файл для тестирования. Он будет активен при сливании. 
 
 using std::cout;
@@ -11,15 +12,30 @@ using std::cin;
 
 
 void tests();
+void testVector1()
+{
+	Toplitz_Matrix::Vector* vec1 = new Toplitz_Matrix::RamVector();
+	int k;
+	for (int i=0;i<(*vec1).size;i++)
+	{
+		k=rand() % 10000 + (-10000);
+		(*vec1)[i]= k;
+		if ((*vec1)[i] != k )
+			cout<<"Ошибка в доступе к элементу с индексом" <<i;
+	}
+}
 
 int main()
 {
+	int a;
+	cin>>a;
 	Toplitz_Matrix::Vector* vec = new Toplitz_Matrix::RamVector();
 	return 0;
 }
 
 void tests()
 {
+	testVector1();
 	//тут должны быть вызваны функции-юнит тестов.
 	//Каждый юнит тест должен быть в отдельной функции. Тут только вызовы. 
 }
